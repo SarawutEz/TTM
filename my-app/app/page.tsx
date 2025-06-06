a7cbba64d62f4d4e6756b1dcee12ccd18c35e55f
Sarawut <125782682+SarawutEz@users.noreply.github.com>
2025-06-06 16:58:29 +0700

    start

---
+++ my-app/app/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function page() {
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline text-amber-400">
+      Hello world!
+    </h1>
+    <button className="btn btn-xs">Xsmall</button>
+    </>
+    
+    
+  )
+}
+
+export default page
