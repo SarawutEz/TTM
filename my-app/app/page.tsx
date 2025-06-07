8e9432c94472b5c0bde890185ac5495d3808d083
Sarawut <125782682+SarawutEz@users.noreply.github.com>
2025-06-07 09:57:33 +0700

    daisy

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,14 +2,7 @@ import React from 'react'
 
 function page() {
   return (
-    <>
-    <h1 className="text-3xl font-bold underline text-amber-400">
-      Hello world!
-    </h1>
-    <button className="btn btn-xs">Xsmall</button>
-    </>
-    
-    
+    <button className="btn btn-xl">Xlarge</button>
   )
 }
 
